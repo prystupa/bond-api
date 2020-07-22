090c6e9aa70da4eb55dfcd859b0cdf2fbeba4fc4
Marcio Granzotto <oscensores@gmail.com>
2020-07-22 20:02:48 -0300

    Update version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="bond_api",
-    version="0.1.4",
+    version="0.1.5",
     packages=find_packages(exclude=['tests']),
 
     author="Eugene Prystupa",
