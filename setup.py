0d04449802f31313963d350c4d023b42de43f3a7
eugene-coast <eugene@coastpay.com>
2021-10-16 17:09:19 -0400

    release new version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="bond_api",
-    version="0.1.13",
+    version="0.1.14",
     packages=find_packages(exclude=['tests']),
 
     author="Eugene Prystupa",
