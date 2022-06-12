2158e18bd071d1ea5cb74c6beebd5a1fc7d48414
Marcio <oscensores@gmail.com>
2022-06-12 19:52:56 -0300

    Bump version number

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="bond_async",
-    version="0.1.20",
+    version="0.1.21",
     packages=find_packages(exclude=['tests']),
 
     author="Olibra LLC",
